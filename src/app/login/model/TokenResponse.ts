5b75fec31dbc2976e4ccc42264046b4e5a75bd90
Ali Oztepe <aliztp7@gmail.com>
2020-09-22 22:10:02 +0300

    first commit

---
+++ src/app/login/model/TokenResponse.ts
@@ -0,0 +1,7 @@
+export class TokenResponse {
+  constructor(public code: number,
+              public message: string,
+              public token: string,
+              public role: string) {
+  }
+}
